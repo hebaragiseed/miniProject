3036fe4a49f51b2ed85cd1ec0b7beb55c531389f
hebaragiseed <hebaragiseed@gmail.com>
2017-10-29 02:57:50 +0900

    async await

--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -2,35 +2,27 @@ import React, { Component } from 'react';
 import './Movie.css';
 import PropTypes from 'prop-types';
 
-class Movie extends Component{
-
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
-  }
-
-  render() {
-    
-    return (
-     <div>
-        <MoviePoster poster={this.props.poster} />
-        <h1>{this.props.title}</h1>        
-     </div>
-    );
-  }
+function Movie({title, poster}) {
+  return(
+    <div>
+      <MoviePoster poster={poster} />
+      <h1>{title}</h1>
+    </div>
+  );
+}
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired
 }
 
-
-
-class MoviePoster extends Component{
-  render() {
-    console.log(this.props)
-    return (
-      <div>
-        <img src={this.props.poster} alt="" />
-      </div>
-    );
-  }
+function MoviePoster({poster}) {
+  return(
+    <div>
+      <img src={poster} alt="" />  
+    </div>
+  );
 }
-
+MoviePoster.propTypes = {
+  poster: PropTypes.string.isRequired
+}
 export default Movie
