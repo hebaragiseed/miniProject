07c3b85c818e8f3b7d0d04c13b20d99932e8ad37
hebaragiseed <hebaragiseed@gmail.com>
2017-11-07 00:31:51 +0900

    complete

--- today-weather/App.js
+++ today-weather/App.js
@@ -1,20 +1,64 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View,Image, ActivityIndicator } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import Weather from './Weather.js';
 
+
+ const API_KEY = '5884da9af8df4e9e907c0d71e234feb7';
 export default class App extends Component {
   state = {
-    isLoaded: true
+    isLoaded: false,
+    error: null,
+    temperature: null,
+    name:null
+  };
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this._getWeather(position.coords.latitude, position.coords.longitude);
+      },
+      error => {
+        this.setState({
+          error: error
+        });
+      }
+    );
   }
+
+  _getWeather(lat, long) {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=${API_KEY}`
+    )
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          temperature: json.main.temp,
+          name: json.weather[0].main,
+          isLoaded: true,
+        });
+        console.log(json)
+  
+      });
+  };
+  // Rain
+  // Clear
+  // Thunderstorm
+  // Clouds
+  // Snow
+  // Drizzle
+  // Haze
+  // Mist
+
   render() {
-    const { isLoaded } = this.state;
+    const { isLoaded, error, temperature, name} = this.state;
     return (
       <View style={styles.container}>
+        <StatusBar hidden={true} />
         {isLoaded ? (
-          <Weather /> 
+          <Weather weatherName={'Haze'} temp={Math.floor(temperature - 273.15)}/> 
         ) : (
           <View style={styles.loading}>
             <Text style={styles.loadingText}>Getting the fucking weather</Text>
+            {error ? <Text style={styles.errorText}>{error}</Text> : null}
           </View>
         )}        
       </View>
@@ -37,7 +81,14 @@ const styles = StyleSheet.create({
   },
   loadingText: {
     fontSize: 38,
-    marginBottom: 100
+    marginBottom: 40
+  },
+    errorText: {
+      color: 'red',
+      backgroundColor: 'transparent',
+      marginBottom: 40
+  }
+   
 
-  } 
 });
+
--- today-weather/Weather.js
+++ today-weather/Weather.js
@@ -1,27 +1,105 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { LinearGradient } from 'expo';
-
-export default class Weather extends Component {
-  render() {    
-    return(
-      <LinearGradient
-        colors={["#00C6FB", "#005BEA"]}
-        style={styles.container}>
-        <View style={styles.upper}>
-          <Text>Icon here</Text>
-          <Text style={styles.temp}>35</Text>          
-        </View>
-        <View style={styles.lower}>
-          <Text style={styles.title}>Raining like a MF</Text>
-          <Text style={styles.subtitle}>For more info look outside</Text>          
-        </View>
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import PropTypes from 'prop-types';
+// export default class Weather extends Component {
+//   render() {    
+//     return(
+//       <LinearGradient
+//         colors={['#00C6FB', '#005BEA']}
+//         style={styles.container}>
+//         <View style={styles.upper}>
+//           <Ionicons color='white' size={144} name='ios-rainy'></Ionicons>
+//           <Text style={styles.temp}>35°</Text>          
+//         </View>
+//         <View style={styles.lower}>
+//           <Text style={styles.title}>Raining like a MF</Text>
+//           <Text style={styles.subtitle}>For more info look outside</Text>          
+//         </View>
         
-      </LinearGradient>
-    );
+//       </LinearGradient>
+//     );
 
-  }
+//   }
+// }
+
+const weatherCases = {
+  Rain: {
+    colors: ['#00c6fb', '#005bea'],
+    title: 'Raining like a MF',
+    subtitle: 'For more info look outside',
+    icon: 'weather-rainy'
+  },
+  Clear: {
+    colors: ['#fef253', '#ff7300'],
+    title: 'Sunny as fuck',
+    subtitle: 'Go get your as burnt',
+    icon: 'weather-sunny'
+  },
+  Thunderstorm: {
+    colors: ['#00ecbc','#007adf'],
+    title: 'Thunderstorm in the house',
+    subtitle: 'Actually, outside of the house',
+    icon: 'weather-ligthning',      
+  },
+  Clouds: {
+    colors: ['#d702cc', '#007adf'],
+    title: 'Clouds',
+    subtitle: 'I know, fucking boring☁️☁️☁️☁️☁️',
+    icon: 'weather-cloudy'
+  },
+  Snow: {
+    colors: ['#7de2fc', '#b9b6e5'],
+    title: 'Snow',
+    subtitle: 'Do you want to build a snowman? Fuck no.',
+    icon: 'weather-snowy'
+  },
+  Drizzle: {
+    colors: ['#89f7fe', '#66a6ff'],
+    tilte: 'Drizzle',
+    subtitle: 'Is like rain',
+    icon: 'weather-hail'  
+  },
+  Haze: {
+    colors: ['#89f7fe', '#66a6ff'],
+    tilte: 'Haze',
+    subtitle: 'Haze🌫🌫🌫🌫',
+    icon: 'weather-fog'  
+  },
+  Mist: {
+    colors: ['#89f7fe', '#66a6ff'],
+    tilte: 'Nist!',
+    subtitle: "It's like you have no glasses on.",
+    icon: 'weather-fog'  
+  },
 }
+
+function Weather({ temp, weatherName }) {
+  console.log(typeof(weatherCases[weatherName]));
+  console.log(typeof(weatherCases.Clouds))
+  return (
+    <LinearGradient 
+      colors={weatherCases[weatherName].colors}
+      style={styles.container}>
+      <View style={styles.upper}>
+        <MaterialCommunityIcons color='white' size={144} name={weatherCases[weatherName].icon}></MaterialCommunityIcons>
+        <Text style={styles.temp}>{temp}°</Text>          
+      </View>
+      <View style={styles.lower}>
+        <Text style={styles.title}>{weatherCases[weatherName].title}</Text>
+        <Text style={styles.subtitle}>{weatherCases[weatherName].subtitle}</Text>          
+      </View>        
+    </LinearGradient>    
+  );
+}
+
+Weather.propTypes = {
+  temp: PropTypes.number.isRequired,
+  weatherName: PropTypes.string.isRequired
+};
+
+export default Weather;
 
 const styles = StyleSheet.create({
   container: {
@@ -34,7 +112,7 @@ const styles = StyleSheet.create({
     backgroundColor: "transparent",
   },
   temp: {
-    fontSize: 38,
+    fontSize: 48,
     backgroundColor: "transparent",
     color: "white",
     marginTop: 10,
